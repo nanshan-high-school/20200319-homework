a071c3b1fe23455a3f632d9785f7227d08d0d1b0
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:32:04 +0800

    新手組-10月-PPAP

--- goodjack .cpp
+++ goodjack .cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 int main() {
-    int n,a[8],b[8],c[8];
+    int n,times = 1;
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        cin >> a[i] >> b[i] >> c[i];
+    while(n >= (4 * times)){
+        n -= (4 * times);
+        times++;
     }
-    bool empty = {true};
-    for(int i = 1; i <= n; i++){
-        empty = false;
-        for(int j = 1; j <= 500; j++){
-            if(a[i] < j && j < b[i] && j % c[i] != 0){
-                cout << j << " ";
-                empty = true;
-            }
-        }
-        if(empty == false){
-            cout << "No free parking spaces.";
-        }
-        cout << "\n";
-    }
+    if(n < times){
+        cout << "Pen";
+    }else if(n < 2 * times){
+        cout << "Pineapple";
+    }else if(n < 3 * times){
+        cout << "Apple";
+    }else if(n < 4 * times){
+        cout << "Pineapple pen";
+    } 
 }
