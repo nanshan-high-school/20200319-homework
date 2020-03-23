746900ddbeb6b055639a1d029d3d889e53e9533a
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:42:07 +0800

    新手組-11月-不再傻傻等公車

--- goodjack .cpp
+++ goodjack .cpp
@@ -2,48 +2,39 @@
 using namespace std;
 
 int main() {
-    int n,h,m,t,p;
+    float m,a,n,e;
+    int day[8] = {0};
+    int time[8] = {0};
+    int maxDay = 0;
+    int maxTime = 0;
 
-    cin >> n;
-    cin >> h >> m;
-
-    int hour[21] = {h};
-    int min[21] = {m};
-
-    for (int i = 1; i <= n; i++){
-        cin >> t;
-        hour[i] = hour[i - 1];
-        min[i] = min[i - 1] + t;
-
-        while (min[i] >= 60){
-            min[i] -= 60;
-            hour[i] += 1;
+    for(int i = 1; i < 7; i++){
+        cin >> m >> a >> n >> e;
+        
+        day[i] = m + a + n + e;
+        time[1] += m;
+        time[2] += a;
+        time[3] += n;
+        time[4] += e;
+    }
+    
+    for(int i = 1; i < 7; i++){
+        if (day[i] > day[maxDay]){
+            maxDay = i;
         }
-
-        while (hour[i] >= 24){
-            hour[i] -= 24;
-        }
+        if (time[i] > time[maxTime]){
+            maxTime = i;
+        } 
     }
 
-    while(n != 0){
-        cin >> p;
-
-        if (p == 0){
-            return 0;
-        }
-
-        cout << "\n";
-
-        if (hour[p] < 10){
-            cout << "0" << hour[p] << ":";
-        } else {
-            cout << hour[p] << ":";
-        }
-
-        if (min [p] < 10){
-            cout << "0" << min [p] << "\n";
-        } else {
-            cout << min [p] << "\n";
-        }
+    cout << "\n" << maxDay << "\n";
+    if (maxTime == 1){
+        cout << "morning";
+    } else if (maxTime == 2){
+        cout << "afternoon";
+    } else if (maxTime == 3){
+        cout << "night";
+    } else {
+        cout << "early morning";
     }
 }
