2b5fcb6339e9b4f59ff3e757b624c935df874ea6
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:06:18 +0800

    新手同好會-數位邏輯運算

--- goodjack .cpp
+++ goodjack .cpp
@@ -2,35 +2,38 @@
 using namespace std;
 
 int main() {
-    char c = '\0',bitmap[63][63] = {0};
-    int n,m,bit;
-    long long s,num;
-
-    cin >> n >> m >> c;
-
-    for (int i = 1; i <= n; i++){
-        num = 2305843009213694000;
-        bit = 62;
-        cin >> s;
-        while(bit > 0){
-            if(s >= num){
-                s -= num;
-                bitmap[i][bit] = 1;
-            }else if(s < num){
-                bitmap[i][bit] = 2;
-            }
-            num /= 2;
-            bit--;
+    int n,t,number[11][11] = {0},AND[11] = {0},OR[11] = {0},XOR[11] = {0};
+    cin >> n >> t;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= t; j++){
+            cin >> number[i][j];
+            AND[j] += number[i][j];
+            OR[j] += number[i][j];
+            XOR[j] += number[i][j];
         }
     }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            if (bitmap[i][j] == 1){
-                cout << c << ' ';
-            }else if(bitmap[i][j] == 2){
-                cout << ". ";
-            }
+    cout << "\nAND: ";
+    for(int j = 1; j <= t; j++){
+        if(AND[j] == n){
+            cout << "1 ";
+        }else if(AND[j] != n){
+            cout << "0 ";
         }
-        cout << "\n";
+    }
+    cout << "\n OR: ";
+    for(int j = 1; j <= t; j++){
+        if(OR[j] > 0){
+            cout << "1 ";
+        }else if(OR[j] <= 0){
+        cout << "0 ";
+        }
+    }
+    cout << "\nXOR: ";
+    for(int j = 1; j <= t; j++){
+        if(XOR[j] % 2 == 1){
+            cout << "1 ";
+        }else if(XOR[j] % 2 == 0){
+            cout << "0 ";
+        }
     }
 }
