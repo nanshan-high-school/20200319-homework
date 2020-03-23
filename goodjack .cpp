7b97fe2370655ce0f14ad101fb57861b1daa9db2
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:52:47 +0800

    2020-03-19

--- goodjack .cpp
+++ goodjack .cpp
@@ -1,26 +1,32 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-int main() {
-    char palindrome[501] = {0};
-    cin >> palindrome;
+int main(){
+    int tester;
+    string subject[6] = {"0","國", "英", "數", "社", "自"};
+    float average,allS[6] = {0};
 
-    int length = strlen(palindrome);
-    int halfLength = length / 2;
-
-    if (length % 2 != 0){
-        cout << "no";
-        return 0;
-    }
-    for(int i = 0; i < halfLength; i++){
-        if(palindrome[i] != palindrome[length - 1 - i]){
-            cout << "no";
-            return 0;
+    cout << "輸入人數:";
+    cin >> tester;
+    cout << "輸入成績\n";
+    int score[tester + 1][6],allP[999999] = {0};
+    for(int i = 1; i <= tester; i++){
+        cout << "第" << i << "人\n";
+        for(int j = 1; j <= 5; j++){
+            cout << subject[j] << ":";
+            cin >> score[i][j];
+            allS[j] += score[i][j];
+            allP[i] += score[i][j];
         }
     }
-    cout << "yes\n";
-    for(int i = 0; i < halfLength; i++){
-        cout << palindrome[i];
+    cout << "各科平均:\n";
+    for(int j = 1; j <= 5; j++){
+        average = allS[j] / tester;
+        cout << average << "\n";
+    }
+    cout << "個人平均\n";
+    for(int j = 1; j <= tester; j++){
+        average = allP[j] / 5.0;
+        cout << "第" << j << "人" << average << "\n";
     }
 }
