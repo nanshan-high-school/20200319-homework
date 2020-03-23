a40ab379f6b95b048f95e0b8d01076d48b2bc1f5
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:51:52 +0800

    新手組-12月-尋寶地圖

--- goodjack .cpp
+++ goodjack .cpp
@@ -1,52 +1,26 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-int main(){
-    int map[101][101] = {0} , change[101][101] = {0} , n , m , changeAdd[101][101] = {0};
+int main() {
+    char palindrome[501] = {0};
+    cin >> palindrome;
 
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            cin >> map[i][j];
+    int length = strlen(palindrome);
+    int halfLength = length / 2;
+
+    if (length % 2 != 0){
+        cout << "no";
+        return 0;
+    }
+    for(int i = 0; i < halfLength; i++){
+        if(palindrome[i] != palindrome[length - 1 - i]){
+            cout << "no";
+            return 0;
         }
     }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            cin >> change[i][j];
-        }
-    }
-    for(int a = 1; a <= n; a++){
-        for(int b = 1; b <= m; b++){
-            for(int d = 1; d <= m; d++){
-                changeAdd[a][b] += change[a][d];
-            }
-        }
-    }
-    for(int a = 1; a <= n; a++){
-        for(int b = 1; b <= m; b++){
-            for(int c = 1; c <= n; c++){
-                changeAdd[a][b] += change[c][b];
-            }
-            changeAdd[a][b] -= change[a][b];
-        }
-    }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            if (map[i][j] == 1){
-                if (changeAdd[i][j] % 2 == 1){
-                    map[i][j] = 0;
-                }
-            }else if (map[i][j] == 0){
-                if (changeAdd[i][j] % 2 == 1){
-                    map[i][j] = 1;
-                }
-            }
-        }
-    }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            cout << map[i][j];
-        }
-        cout << "\n";
+    cout << "yes\n";
+    for(int i = 0; i < halfLength; i++){
+        cout << palindrome[i];
     }
 }
