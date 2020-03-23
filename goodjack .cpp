b4dfa0b12898c881e02eae0af2a76122a2cbf001
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:40:21 +0800

    新手組-10月-虛擬寵物大師

--- goodjack .cpp
+++ goodjack .cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 int main() {
-    int n,times = 1;
-    cin >> n;
-    while(n >= (4 * times)){
-        n -= (4 * times);
-        times++;
+    int n,s,cp[51] = {0},iv[51] = {0},upgrade,best = 0;
+    cin >> n >> s;
+    upgrade = s / 1000;
+    for(int i = 1; i <= n; i++){
+        cin >> cp[i] >> iv[i];
+        if(iv[i] >= 0 && iv[i] <= 29){
+            cp[i] += 10 * upgrade;
+        }else if(iv[i] >= 30 && iv[i] <= 39){
+            cp[i] += 50 * upgrade;
+        }else if(iv[i] >= 40 && iv[i] <= 45){
+            cp[i] += 100 * upgrade;
+        }
+        if(cp[i] > cp[best]){
+            best = i;
+        }
     }
-    if(n < times){
-        cout << "Pen";
-    }else if(n < 2 * times){
-        cout << "Pineapple";
-    }else if(n < 3 * times){
-        cout << "Apple";
-    }else if(n < 4 * times){
-        cout << "Pineapple pen";
-    } 
+    cout << best << " " << cp[best];
 }
