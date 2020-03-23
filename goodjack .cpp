71a3af33278038b5b63044513acbfdd4069121a3
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2020-03-23 23:47:11 +0800

    新手組-11月-多項式計算

--- goodjack .cpp
+++ goodjack .cpp
@@ -2,39 +2,36 @@
 using namespace std;
 
 int main() {
-    float m,a,n,e;
-    int day[8] = {0};
-    int time[8] = {0};
-    int maxDay = 0;
-    int maxTime = 0;
+    int N1;
+    int N2;
+    cin >> N1;
 
-    for(int i = 1; i < 7; i++){
-        cin >> m >> a >> n >> e;
-        
-        day[i] = m + a + n + e;
-        time[1] += m;
-        time[2] += a;
-        time[3] += n;
-        time[4] += e;
+    int M[1001] = {0};
+    long long C;
+    int P;
+
+    for(int i = 0; i < N1; i++){
+        cin >> P;
+        cin >> C;
+        M[P] += C;
     }
-    
-    for(int i = 1; i < 7; i++){
-        if (day[i] > day[maxDay]){
-            maxDay = i;
+
+    cin >> N2;
+
+    for(int i = 0; i < N2; i++){
+        cin >> P;
+        cin >> C;
+        M[P] += C;
+    }
+    int flag = 0;
+    for(int i = 1000 ;i >= 0; i--){
+        if(M[i] != 0){
+            cout << i << ":" << M[i] << "\n";
+        } else {
+            flag = 1;
         }
-        if (time[i] > time[maxTime]){
-            maxTime = i;
-        } 
     }
-
-    cout << "\n" << maxDay << "\n";
-    if (maxTime == 1){
-        cout << "morning";
-    } else if (maxTime == 2){
-        cout << "afternoon";
-    } else if (maxTime == 3){
-        cout << "night";
-    } else {
-        cout << "early morning";
-    } 
+    while (flag == 0){
+        cout << "NULL!";
+    }
 }
